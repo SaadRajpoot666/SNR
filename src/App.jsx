2f2135a7bfe302d782a00e7a3a8180bc992457d2
Saad Bin Khalid <khalidmehmoodeditz@gmail.com>
2025-06-05 22:51:19 +0500

    Setting up Projects Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { About } from "./components/About/About"
 import { Footer } from "./components/Footer/Footer"
 import { Hero } from "./components/HeroSection/Hero"
 import { Navbar } from "./components/Navbar/Navbar"
+import { Projects } from "./components/Projects/Projects"
 import { Services } from "./components/Services/Services"
 
 function App() {
@@ -10,6 +11,7 @@ return <>
 <Hero   />
 <About   />
 <Services   />
+<Projects   />
 <Footer   />
 
 </>
